6719078a90f0b87222f71e9419ba0ad39657b813
FaYaZ024 <147800959+FaYaZ024@users.noreply.github.com>
2025-11-03 10:26:35 +0530

    Update Header.jsx

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
 
   useEffect(() => {
     axios
-      .get("https://graphizinebackend.onrender.com/api/categories")
+      .get("http://localhost:8080/api/categories")
       .then((res) => setCategories(res.data))
       .catch((err) => console.error("Error fetching categories:", err));
   }, []);
